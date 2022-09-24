ce2b53ed810b72427b4d44cb2e4103f0fab1fb20
VladlenT <vladlent.dev@gmail.com>
2022-09-24 12:07:19 +0200

    feat: adds Stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,36 @@
+type Node<T> = {
+    val: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
-    public length: number;
+    length = 0;
 
-    
-
-    constructor() {
-    }
+    private head?: Node<T>;
 
     push(item: T): void {
+        this.length++;
 
-}
+        const node: Node<T> = { val: item };
+
+        if (!this.head) {
+            this.head = node;
+        } else {
+            node.prev = this.head;
+            this.head = node;
+        }
+    }
+
     pop(): T | undefined {
+        if (!this.head) return;
+        this.length--;
 
-}
+        const val = this.head.val;
+        this.head = this.head.prev;
+        return val;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
