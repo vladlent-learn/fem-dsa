13cbd94223c4b605b5d89c595ffb271add82f098
VladlenT <vladlent.dev@gmail.com>
2022-09-25 13:20:15 +0200

    feat: adds implementation for MazeSolver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,33 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    return walk(maze, wall, start, end, []);
+}
 
+function walk(
+    maze: string[],
+    wall: string,
+    current: Point,
+    end: Point,
+    result: Point[],
+): Point[] {
+    if (current.y >= maze.length || current.x >= maze[end.y].length)
+        return result;
+    if (current.x < 0 || current.y < 0) return result;
+    if (maze[current.y][current.x] === wall) return result;
+    if (result.find((p) => current.x === p.x && current.y === p.y))
+        return result;
+
+    result.push(current);
+
+    if (current.x === end.x && current.y === end.y) return result;
+
+    walk(maze, wall, { x: current.x - 1, y: current.y }, end, result);
+    walk(maze, wall, { x: current.x + 1, y: current.y }, end, result);
+    walk(maze, wall, { x: current.x, y: current.y + 1 }, end, result);
+
+    return result;
 }
