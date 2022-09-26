c151086a243e3c1e151b5875565bc6a1a8c7438a
VladlenT <vladlent.dev@gmail.com>
2022-09-26 13:18:21 +0200

    feat: adds implementation for CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,13 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (!a && !b) return true;
+    if (!a || !b) return false;
+    if (a?.value !== b?.value) return false;
 
+    const left = compare(a!.left, b!.left);
+    const right = compare(a!.right, b!.right);
+
+    return left && right;
 }
