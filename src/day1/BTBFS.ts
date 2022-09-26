2f663472262b6b2ec86c4697bab3ae8e8ede121d
VladlenT <vladlent.dev@gmail.com>
2022-09-26 13:00:05 +0200

    feat: adds implementation for Breadth First Search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,18 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "@code/Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue = new Queue<BinaryNode<number>>();
+
+    queue.enqueue(head);
+
+    while (queue.length !== 0) {
+        const curr = queue.deque();
+
+        if (curr?.value === needle) return true;
+
+        if (curr?.left) queue.enqueue(curr.left);
+        if (curr?.right) queue.enqueue(curr.right);
+    }
+
+    return false;
 }
