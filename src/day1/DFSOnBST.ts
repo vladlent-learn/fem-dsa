2059e50183f66cc5631a989713324a4456a911fd
VladlenT <vladlent.dev@gmail.com>
2022-09-26 15:57:35 +0200

    feat: adds implementation for Depth First Search on Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
+}
 
+function search(head: BinaryNode<number> | null, needle: number): boolean {
+    if (!head) return false;
+
+    if (head.value === needle) return true;
+
+    if (head.value < needle) return search(head.right, needle);
+
+    return search(head.left, needle);
 }
