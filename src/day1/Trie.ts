cc10f2dcca06edff3ce64d2c53cff7e41d042fd3
VladlenT <vladlent.dev@gmail.com>
2022-09-27 15:51:14 +0200

    feat: adds implementation for "insert" method for "Trie"

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,53 @@
+export class Node {
+    isWord = false;
+    children: Node[] = [];
+
+    constructor(public val: string) {}
+}
+
 export default class Trie {
-    
+    private abc: Node[] = Array.from(
+        { length: 26 },
+        (_, i) => new Node(String.fromCharCode(i)),
+    );
 
-    
-
-    constructor() {
+    private getIndex(char: string): number {
+        return char.charCodeAt(0) - "a".charCodeAt(0);
     }
 
     insert(item: string): void {
+        let curr = this.abc[this.getIndex(item[0])];
 
-}
-    delete(item: string): void {
+        for (let i = 1; i < item.length; i++) {
+            const idx = this.getIndex(item[i]);
+            let next = curr.children[idx];
 
-}
+            if (!next) {
+                next = new Node(item[i]);
+                curr.children[idx] = next;
+            }
+
+            curr = next;
+        }
+
+        curr.isWord = true;
+    }
+
+    delete(item: string): void {}
+
     find(partial: string): string[] {
+        const result = [];
+        return [];
+    }
 
+    toString(): string {
+        return JSON.stringify(this.abc, null, 2);
+    }
 }
-}
+
+const trie = new Trie();
+trie.insert("foo");
+trie.insert("fool");
+trie.insert("foolish");
+trie.insert("bar");
+console.log(trie.toString());
