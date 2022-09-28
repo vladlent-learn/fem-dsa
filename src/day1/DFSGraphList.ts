5fcc654b06a03ca4e1293ead4d63b97f0dbc83bc
VladlenT <vladlent.dev@gmail.com>
2022-09-28 10:34:40 +0200

    feat: adds implementation for DFS Graph List

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,40 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const result: number[] = [];
 
+    if (traverse(graph, source, needle, [], result)) {
+        return result;
+    } else {
+        return null;
+    }
+}
+
+function traverse(
+    graph: WeightedAdjacencyList,
+    current: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (seen[current]) return false;
+
+    path.push(current);
+    seen[current] = true;
+
+    if (needle === current) {
+        return true;
+    }
+
+    for (const node of graph[current]) {
+        if (traverse(graph, node.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    path.pop();
+
+    return false;
 }
