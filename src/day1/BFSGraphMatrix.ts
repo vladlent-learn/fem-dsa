3a62758083931e77e51c7315b0a045e241f3b3f1
VladlenT <vladlent.dev@gmail.com>
2022-09-28 09:56:22 +0200

    feat: adds implementation for BFS Graph Matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,41 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "@code/Queue";
 
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+    const prev = new Array(graph.length).fill(-1);
+    const q = new Queue<number>();
+
+    q.enqueue(source);
+    seen[source] = true;
+
+    while (q.length) {
+        const curr = q.deque() as number;
+
+        if (curr === needle) break;
+
+        const adj = graph[curr];
+        for (let i = 0; i < adj.length; i++) {
+            if (adj[i] === 0 || seen[i]) continue;
+
+            q.enqueue(i);
+            seen[i] = true;
+            prev[i] = curr;
+        }
+    }
+
+    if (prev[needle] === -1) return null;
+
+    const result = [];
+    let current = needle;
+
+    while (prev[current] !== -1) {
+        result.push(current);
+        current = prev[current];
+    }
+
+    return [source, ...result.reverse()];
 }
