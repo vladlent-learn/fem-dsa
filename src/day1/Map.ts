39e744617d78d98e411a6481993b5ac1056d94ee
VladlenT <vladlent.dev@gmail.com>
2022-09-30 16:22:11 +0200

    feat: adds implementation for Maps

--- src/day1/Map.ts
+++ src/day1/Map.ts
@@ -1,21 +1,41 @@
-export default class Map<T extends (string | number), V> {
-    
+export default class Map<T extends string | number, V> {
+    private list: (V | undefined)[] = new Array(15);
+    private length = 0;
 
-    
+    private hash(key: string | number): number {
+        if (typeof key === "string") {
+            let sum = 0;
 
-    constructor() {
+            for (let i = 0; i < key.length; i++) {
+                sum += key.charCodeAt(i);
+            }
+
+            return sum % this.list.length;
+        }
+        return key & this.list.length;
     }
 
     get(key: T): V | undefined {
-
-}
+        return this.list[this.hash(key)];
+    }
     set(key: T, value: V): void {
+        this.length++;
+        this.list[this.hash(key)] = value;
+    }
 
-}
     delete(key: T): V | undefined {
+        const hash = this.hash(key);
+        const value = this.get(key);
 
-}
+        if (!value) return undefined;
+
+        this.length--;
+        this.list[hash] = undefined;
+
+        return value;
+    }
+
     size(): number {
-
-}
+        return this.length;
+    }
 }
